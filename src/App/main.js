b044122cdfb342648983de6fc19d20482a652859
garrychiao <g50905g@gmail.com>
2020-05-01 00:25:17 +0800

    yahoo tracking removed

--- src/App/main.js
+++ src/App/main.js
@@ -659,19 +659,6 @@ $(function(){
 				.then(() => {_.renderChart()})
 				.then(() => {this.show()})
 
-			// yahoo tracking
-			window.dotq = window.dotq || [];
-			window.dotq.push({
-				'projectId': '10000',
-				'properties': {
-					'pixelId': '10094925',
-					'qstrings': {
-					'et': 'custom',
-					'ea': 'submit'
-					}
-				} 
-			});
-
 		},
 		fetchChartData: () => {
 			var _ = this;
